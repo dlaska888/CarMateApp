3630486ed8a937fb269972291808e2f45d8475b5
Dawid Laska <dawidlaska888@gmail.com>
2024-04-16 17:39:41 +0200

    added settings page

--- flutter_frontend/lib/screens/dashboard_pages/my_cars.dart
+++ flutter_frontend/lib/screens/dashboard_pages/my_cars.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyCarsPage extends StatelessWidget {
   const MyCarsPage({super.key});
@@ -6,145 +7,176 @@ class MyCarsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        mainAxisAlignment: MediaQuery.of(context).size.width < 768
-            ? MainAxisAlignment.start
-            : MainAxisAlignment.center,
-        children: [
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 600),
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white, 
-                  borderRadius: BorderRadius.circular(
-                      10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2.0,
-                      blurRadius: 4.0,
-                      offset:
-                          const Offset(2.0, 2.0),
+      child: SingleChildScrollView(
+        child: Wrap(
+          alignment: MediaQuery.of(context).size.width < 768
+              ? WrapAlignment.start
+              : WrapAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 700),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: const Offset(2.0, 2.0),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                margin: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    margin: const EdgeInsets.all(8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "My car name",
-                            style: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "My car name",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text("Hyundai i30 2012"),
+                              Text("1.6 Gas 90hp"),
+                              Text("56 000km"),
+                              Text("Bought 12.06.2014")
+                            ],
                           ),
-                          Text("Hyundai i30 2012"),
-                          Text("1.6 Gas 90hp"),
-                          Text("56 000km"),
-                          Text("Bought 12.06.2014")
+                          const SizedBox(width: 64.0),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "images/car1.jpg",
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                      Column(
-                        children: [Image.asset("images/car1.jpg", height: 100)],
-                      )
-                    ],
-                  ),
-                )),
-          ),
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 600),
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white, 
-                  borderRadius: BorderRadius.circular(
-                      10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2.0,
-                      blurRadius: 4.0,
-                      offset:
-                          const Offset(2.0, 2.0),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 700),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: const Offset(2.0, 2.0),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                margin: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    margin: const EdgeInsets.all(8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "My car name",
-                            style: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "My car name",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text("Hyundai i30 2012"),
+                              Text("1.6 Gas 90hp"),
+                              Text("56 000km"),
+                              Text("Bought 12.06.2014")
+                            ],
                           ),
-                          Text("Hyundai i30 2012"),
-                          Text("1.6 Gas 90hp"),
-                          Text("56 000km"),
-                          Text("Bought 12.06.2014")
+                          const SizedBox(width: 64.0),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "images/car1.jpg",
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                      Column(
-                        children: [Image.asset("images/car1.jpg", height: 100)],
-                      )
-                    ],
-                  ),
-                )),
-          ),
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 600),
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white, 
-                  borderRadius: BorderRadius.circular(
-                      10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2.0,
-                      blurRadius: 4.0,
-                      offset:
-                          const Offset(2.0, 2.0),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 700),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: const Offset(2.0, 2.0),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                margin: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    margin: const EdgeInsets.all(8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "My car name",
-                            style: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "My car name",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text("Hyundai i30 2012"),
+                              Text("1.6 Gas 90hp"),
+                              Text("56 000km"),
+                              Text("Bought 12.06.2014")
+                            ],
                           ),
-                          Text("Hyundai i30 2012"),
-                          Text("1.6 Gas 90hp"),
-                          Text("56 000km"),
-                          Text("Bought 12.06.2014")
+                          const SizedBox(width: 64.0),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "images/car1.jpg",
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                      Column(
-                        children: [Image.asset("images/car1.jpg", height: 100)],
-                      )
-                    ],
-                  ),
-                )),
-          ),
-          
-        ],
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- flutter_frontend/lib/screens/dashboard_pages/settings.dart
+++ flutter_frontend/lib/screens/dashboard_pages/settings.dart
@@ -5,12 +5,131 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Expanded(child: Text("setting1")),
-        Expanded(child: Text("setting2")),
-        Expanded(child: Text("setting3")),
-      ],
+    final primary = Theme.of(context).primaryColor;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: screenWidth > 768
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.start,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: Wrap(
+                alignment: screenWidth > 768
+                    ? WrapAlignment.spaceEvenly
+                    : WrapAlignment.center,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 500),
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color:
+                                  Colors.white, // Set background color (optional)
+                              borderRadius: BorderRadius.circular(
+                                  20.0), // Add rounded corners (optional)
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey
+                                      .withOpacity(0.5), // Shadow color
+                                  spreadRadius:
+                                      2.0, // Adjusts shadow spread (optional)
+                                  blurRadius:
+                                      4.0, // Adjusts shadow blur (optional)
+                                  offset: const Offset(
+                                      2.0, 2.0), // Sets shadow offset (optional)
+                                ),
+                              ],
+                            ),
+                            padding: const EdgeInsets.all(64.0),
+                            child: Column(
+                              children: [
+                                const CircleAvatar(
+                                  radius: 150,
+                                  backgroundImage: AssetImage("images/car1.jpg"),
+                                ),
+                                const SizedBox(height: 32),
+                                Column(children: [
+                                  Text("Your name",
+                                      style: TextStyle(
+                                          fontSize: 32.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: primary)),
+                                  Text("email@gmail.com",
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: primary)),
+                                ]),
+                              ],
+                            ),
+                            // ... other Container properties (width, height, child)
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ConstrainedBox(
+                      constraints:
+                          const BoxConstraints(maxWidth: 600, maxHeight: 400),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Change username"),
+                            ),
+                            
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Change Password"),
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Resend Verification Email"),
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Enable 2FA"),
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: (){},
+                              child: const Text("Delete Account"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
